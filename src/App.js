e769ccb0b2f9f30ce976ec7d214b824c46bf3fd3
“maxwilsonpereira <maxwilsonpereira@gmail.com>
2020-05-17 17:57:44 +0200

    App.js updated

--- src/App.js
+++ src/App.js
@@ -63,7 +63,6 @@ const app = (props) => {
   }
   return (
     <div>
-      CHANGES
       <Layout>
         {/* You could render a Spinner on fallback. */}
         <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
